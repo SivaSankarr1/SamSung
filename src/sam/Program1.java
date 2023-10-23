95c9d1c8c9cd2491b3fe22b8fec2ac4bb8a1e335
SivaSankarr1 <sivasankarr1@outlook.com>
2023-10-24 00:45:28 +0530

    B work updated

--- src/sam/Program1.java
+++ src/sam/Program1.java
@@ -3,7 +3,9 @@ package sam;
 public class Program1 {
 
 	public static void main(String[] args) {
+		String s=new String("SIVA");
 		System.out.println("Sample_Test_Program_1");
+		System.out.println(s.endsWith("VA"));
 	}
 
 }
